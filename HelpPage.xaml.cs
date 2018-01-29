618b44936bb141620fc7ec432675abd3ae02dbd2
kiranaluru80 <31380802+kiranaluru80@users.noreply.github.com>
2018-01-29 07:11:36 -0500

    jan28

---
+++ HelpPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ConEd.PAP.Views
+{
+    public partial class HelpPage : ContentPage
+	{
+		public string iscomefrom;
+		public HelpPage()
+        {
+            InitializeComponent();
+            Title = "HELP";
+            var navigationPage = Application.Current.MainPage as NavigationPage;
+            navigationPage.BarBackgroundColor = Color.FromHex("#43AEE7");
+            NavigationPage.SetBackButtonTitle(this, "Back");
+            navigationPage.BarTextColor = Color.White;
+            closeref.Clicked += (object sender, EventArgs e) =>
+            {
+                Navigation.PushAsync(new DocumentTypes(iscomefrom));
+            };
+
+		}
+    }
+}
